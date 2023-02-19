01b417bcecccd28ff2460dd734ee8fe7f8e0c4b5
Thang Kieu <thang.kieu@2359media.com>
2023-02-19 14:41:04 +0800

    Update theme

--- app/(client)/components/App.tsx
+++ app/(client)/components/App.tsx
@@ -1,14 +1,19 @@
 'use client';
 
-import { Layout } from 'antd';
+import { ConfigProvider, Layout } from 'antd';
 import { memo } from 'react';
 import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
+import { configProviders } from '../antd-theme';
 import { theme } from '../theme';
 
 interface Props {
   children: React.ReactNode;
 }
 export const App = memo<Props>(function BasicLayout(props) {
-  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <ConfigProvider {...configProviders}>{props.children}</ConfigProvider>
+    </ThemeProvider>
+  );
 });
