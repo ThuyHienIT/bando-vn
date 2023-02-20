b6f51ddba13af9b45a914058957e3deb506bdb30
Thang Kieu <thang.kieu@2359media.com>
2023-02-20 20:50:48 +0800

    Update dbmodel methods

--- pages/models/db.ts
+++ pages/models/db.ts
@@ -1,32 +1,50 @@
-import path from 'path';
-import { promises as fs } from 'fs';
-import dayjs from 'dayjs';
 import { randomUUID } from 'crypto';
+import dayjs from 'dayjs';
+import { promises as fs } from 'fs';
+import path from 'path';
+
 import { tryParseJson } from '../lib/tryParseJSON';
 
+const IS_TEST = process.env.NODE_ENV === 'test';
 const DB_FOLDER = process.env.NODE_ENV === 'test' ? 'dbtest' : 'db';
 const DB_DIR = path.join(process.cwd(), DB_FOLDER);
+const TEST_UTILS_DB: Record<string, string> = {};
+
+async function readFile(dbName: string) {
+  if (IS_TEST) return TEST_UTILS_DB[dbName];
+  return await fs.readFile(path.resolve(DB_DIR, dbName), 'utf-8');
+}
+
+async function writeFile(dbName: string, data: string) {
+  if (IS_TEST) {
+    TEST_UTILS_DB[dbName] = data;
+    return;
+  }
+
+  return await fs.writeFile(path.resolve(DB_DIR, dbName), data, 'utf-8');
+}
 
 type RecordType = { id: string; createdAt?: string; updatedAt?: string };
 
 async function prepareDb(dbName: string) {
-  const filePath = path.resolve(DB_DIR, dbName);
+  if (IS_TEST) {
+    TEST_UTILS_DB[dbName] = '[]';
+    return;
+  }
+
   try {
-    const data = await fs.readFile(filePath, 'utf-8');
+    await readFile(dbName);
   } catch (e: any) {
-    await fs.writeFile(filePath, '[]');
+    await writeFile(dbName, '[]');
   }
 }
 
 async function clearDb(dbName: string) {
-  await fs.writeFile(path.resolve(DB_DIR, dbName), '[]', 'utf-8');
+  await writeFile(dbName, '[]');
 }
 
 async function loadDb<T>(dbName: string) {
-  const fileContent = await fs.readFile(path.resolve(DB_DIR, dbName), {
-    encoding: 'utf-8',
-    flag: 'rs+',
-  });
+  const fileContent = await readFile(dbName);
   const data: T[] = tryParseJson(fileContent, []);
 
   return data;
@@ -35,12 +53,7 @@ async function loadDb<T>(dbName: string) {
 async function insertOne<T extends RecordType>(dbName: string, payload: T) {
   const dataToInsert = { ...payload };
 
-  const fileContent = await fs.readFile(path.resolve(DB_DIR, dbName), {
-    encoding: 'utf-8',
-    flag: 'rs+',
-  });
-
-  const data: T[] = tryParseJson(fileContent, []);
+  const data = await loadDb<T>(dbName);
 
   dataToInsert.id = dataToInsert.id || randomUUID();
   dataToInsert.createdAt = dayjs(new Date()).format();
@@ -48,7 +61,7 @@ async function insertOne<T extends RecordType>(dbName: string, payload: T) {
 
   data.push(dataToInsert);
 
-  await fs.writeFile(path.resolve(DB_DIR, dbName), JSON.stringify(data, null, 2), 'utf-8');
+  await writeFile(dbName, JSON.stringify(data, null, 2));
 
   return dataToInsert;
 }
@@ -56,21 +69,16 @@ async function insertOne<T extends RecordType>(dbName: string, payload: T) {
 async function update<T extends RecordType>(dbName: string, payload: T) {
   const dataToInsert = { ...payload };
 
-  const fileContent = await fs.readFile(path.resolve(DB_DIR, dbName), {
-    encoding: 'utf-8',
-    flag: 'rs+',
-  });
-  const data: T[] = tryParseJson(fileContent, []);
+  const data = await loadDb<T>(dbName);
+
   const foundIdx = data.findIndex((i) => i.id === dataToInsert.id);
 
   dataToInsert.updatedAt = dayjs(new Date()).format();
   data.splice(foundIdx, 1, dataToInsert);
 
-  await fs.writeFile(path.resolve(DB_DIR, dbName), JSON.stringify(data, null, 2), 'utf-8');
+  await writeFile(dbName, JSON.stringify(data, null, 2));
 
   return dataToInsert;
 }
 
 export const dbModel = { prepareDb, clearDb, loadDb, insertOne, update };
-
-function readFile(path: string) {}
