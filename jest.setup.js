c0fb42817be51924f68dbcbb5cd0f9a21379d594
Thang Kieu <thang.kieu@2359media.com>
2023-02-23 19:24:28 +0800

    Ignore act warning

--- jest.setup.js
+++ jest.setup.js
@@ -19,3 +19,20 @@ jest.mock('antd', () => {
     notification,
   };
 });
+
+// ignore this warning due to antd issue
+// the tests are working fine but still get this error
+// find a solution for this later
+jest.spyOn(console, 'error').mockImplementation((...args) => {
+  const message = typeof args[0] === 'string' ? args[0] : '';
+  if (
+    message.includes(
+      'When testing, code that causes React state updates should be wrapped into act(...)'
+    ) ||
+    message.includes('antd')
+  ) {
+    return;
+  }
+
+  return consoleError.call(console, args);
+});
