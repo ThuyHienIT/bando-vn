8986ffdabd3dad656ff8fae3e8e844d692c5a889
HienNNT <thuyhienit@gmail.com>
2023-05-14 17:11:39 +0700

    Update Map.tsx

--- src/pages/HomePage/components/Map/Map.tsx
+++ src/pages/HomePage/components/Map/Map.tsx
@@ -104,7 +104,7 @@ const MapComp = memo(() => {
 
       const view = new MapView({
         map: map,
-        center: [10.811743, 106.6061775],
+        center: [106.71852990620054, 10.841172223862083],
         zoom: 10,
         background: { color: '#a7d6fe' },
         container: mapElRef.current,
